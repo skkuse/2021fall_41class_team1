a9001f78ef488833368a85dc504b2300f8c9d4de
박서왕 <sking147@naver.com>
2021-11-28 03:25:47 +0900

    ma upload

---
+++ src/Assets/01_AssetStore/teleportinteractsharp.cs
@@ -0,0 +1,15 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class teleportinteractsharp : UdonSharpBehaviour
+{
+    public Transform Target;
+    public override void Interact()
+    {
+        Networking.LocalPlayer.TeleportTo(Target.position, Target.rotation);
+
+    }
+}
