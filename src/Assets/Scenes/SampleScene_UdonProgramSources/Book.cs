d227ce3456f0660603797f48045e259c47b8fef0
Yebin11 <dpqls2062@gmail.com>
2021-11-30 03:13:04 +0900

    add book text"

---
+++ src/Assets/Scenes/SampleScene_UdonProgramSources/Book.cs
@@ -0,0 +1,31 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+
+public class Book : UdonSharpBehaviour
+{
+    public GameObject bookcase;
+    public GameObject canvas;
+    public Text canvas_text;
+    public Text information;
+    public GameObject book_open;
+
+    void Start()
+    {
+        bookcase = gameObject.GetComponentsInParent<Transform>()[1].gameObject;
+        canvas = bookcase.GetComponentInChildren<Canvas>(true).gameObject;
+        canvas_text = canvas.GetComponentInChildren<Text>(true);
+        information = gameObject.GetComponentInChildren<Text>();
+        Transform[] temp = bookcase.GetComponentsInChildren<Transform>(true);
+        book_open = temp[temp.Length - 1].gameObject;
+    }
+
+    private void OnMouseDown() {
+        canvas.SetActive(true);
+        book_open.SetActive(true);
+        canvas_text.text = information.text;
+    }
+}
---
+++ src/Assets/Scenes/SampleScene_UdonProgramSources/BookOpen.cs
@@ -0,0 +1,23 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class BookOpen : UdonSharpBehaviour
+{
+    public GameObject canvas;
+    void Start()
+    {
+        GameObject bookcase = gameObject.GetComponentsInParent<Transform>()[1].gameObject;
+        canvas = bookcase.GetComponentInChildren<Canvas>(true).gameObject;
+
+        canvas.SetActive(false);
+        gameObject.SetActive(false);
+    }
+
+    private void OnMouseDown() {
+        canvas.SetActive(false);
+        gameObject.SetActive(false);
+    }
+}
